e1f685241c57833ffa4d665ac94bdb4801056b0a
somto <somto565@gmail.com>
2024-10-20 21:30:50 +0100

    added question cataloge

---
+++ src/Helpers/Questionbank.js
@@ -0,0 +1,26 @@
+export const Questions = [
+    {
+    prompt: "What is 1+3?",
+    optionA: "24",
+    optionB: "4",
+    optionC: "5",
+    optionD: "6",
+    answer: "optionB"
+    },
+    {
+    prompt: "Which is a Streaming Platform?",
+    optionA: "Twitter",
+    optionB: "Twitch",
+    optionC: "Twitch",
+    optionD: "Facebook",
+    answer: "optionC"
+    },
+    {
+    prompt: "Best Tool For Frontend?",
+    optionA: "React",
+    optionB: "Django",
+    optionC: "Node",
+    optionD: "Tailwind",
+    answer: "optionA"
+    },  
+];
